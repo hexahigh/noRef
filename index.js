9b17d6f8813ecedbc5127ddf1bf9aaf43af03f4c
Boof <97455552+hexahigh@users.noreply.github.com>
2023-08-23 14:50:40 +0200

    a

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const currentUrl = window.location.href
 
 var url = new URL(currentUrl);
 var c = url.searchParams.get("url");
+var o = url.searchParams.get("obf");
 
 // Check that there is something there otherwhise it will get stuck in a loop
 if(c) {
+    // Check if url is obfuscated
+    if (obf) {
+        c = atob(c)
+    }
     if (c.startsWith("http://") || c.startsWith("https://")) {
         window.location = (c)
     } else {
