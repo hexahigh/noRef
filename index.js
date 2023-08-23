e6d2ef9b6fdf4c7cbf49207d62a702808a3bc9cc
Boof <97455552+hexahigh@users.noreply.github.com>
2023-08-23 14:35:58 +0200

    put it in the wrong place

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var c = url.searchParams.get("url");
 // Check that there is something there otherwhise it will get stuck in a loop
 if(c) {
     if (c.startsWith("http://") || c.startsWith("https://")) {
-        c = "https://" + c
+        window.location = (c)
+    } else {
+        c = "http://" + c
+        window.location = (c)
     }
-    window.location = (c)
 }
