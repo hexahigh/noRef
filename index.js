20c4963ca9b2f00a637e644efd88cb9c819d1ee2
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-08-23 15:27:15 +0200

    a

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const currentUrl = window.location.href
 var url = new URL(currentUrl);
 var c = url.searchParams.get("url");
 var o = url.searchParams.get("obf");
+var n = url.searchParams.get("no");
 
 // Check that there is something there otherwhise it will get stuck in a loop
 if(c) {
@@ -11,9 +12,19 @@ if(c) {
         c = atob(c)
     }
     if (c.startsWith("http://") || c.startsWith("https://")) {
-        window.location = (c)
+        console.log("url:", c)
+        ifN(c)
     } else {
         c = "http://" + c
-        window.location = (c)
+        console.log("url:", c)
+        ifN(c)
+    }
+} else {
+    console.log("No url found, not redirecting")
+}
+
+function ifN(i) {
+    if (!n) {
+        window.location = i
     }
 }
