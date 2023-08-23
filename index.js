9c006ce778748122f29845debef794faa3d9ea87
Boof <97455552+hexahigh@users.noreply.github.com>
2023-08-23 14:27:33 +0200

    fixed?

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const currentUrl = window.location.href
 
 var url = new URL(currentUrl);
 var c = url.searchParams.get("url");
+
+// Check that there is something there otherwhise it will get stuck in a loop
+if(c) {
 window.location.href = (c)
+}
